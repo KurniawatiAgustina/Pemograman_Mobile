4fd6f339a8b3ffc6a22756dd1fb609c1b6c669ec
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-10-26 10:02:56 +0700

    testing

--- UTS/praktikum_uts/lib/item.dart
+++
@@ -1,9 +0,0 @@
-class Item {
-  String name;
-  int price;
-  int stock;
-  String image;
-  double rating;
-
-  Item({required this.name, required this.price, required this.stock, required this.image, required this.rating});
-}
